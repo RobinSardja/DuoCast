381b8487827066505b1aa3e208c6560e962f1b08
RobinSardja <robinsardja@gmail.com>
2025-04-05 12:25:10 -0400

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 
+import 'chat.dart';
+import 'profile.dart';
+import 'settings.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int currPage = 1;
+  PageController pageController = PageController( initialPage: 1 );
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(
+          centerTitle: true
+        )
       ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text( "DuoCast" )
+        ),
+        body: PageView(
+          controller: pageController,
+          onPageChanged: (selectedPage) => setState( () => currPage = selectedPage ),
+          children: [
+            Profile(),
+            Chat(),
+            Settings()
+          ]
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currPage,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon( Icons.person ),
+              label: "Profile"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon( Icons.message ),
+              label: "Chat"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon( Icons.settings ),
+              label: "Settings"
+            )
+          ],
+          onTap: (selectedPage) {
+            setState( () => currPage = selectedPage );
+            pageController.jumpToPage( selectedPage );
+          },
+        ),
+      )
     );
   }
 }
