b834a0893e9a112917f23ef57b07835f4dc715fe
RobinSardja <robinsardja@gmail.com>
2025-04-05 20:31:16 -0400

    minor code review

--- lib/data.dart
+++ lib/data.dart
@@ -3,7 +3,7 @@ class DefaultData {
   final foreignLanguage = "spanish";
   final nativeLanguage = "english";
   final speechPitch = 1.0;
-  final speechRate = 1.0;
+  final speechRate = 0.5;
   final speechVolume = 1.0;
 }
 
